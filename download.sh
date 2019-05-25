6d949222cd830dccf2175126241d4b580dc3233c
Tang Ruixue <51012864+zaynmi@users.noreply.github.com>
2019-05-25 14:03:46 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 mkdir -p data/raw
 cd data/raw
 
-echo "Downloading CLEVR dataset..."
-wget https://s3-us-west-1.amazonaws.com/clevr/CLEVR_v1.0.zip
-unzip CLEVR_v1.0.zip
-rm CLEVR_v1.0.zip
+#echo "Downloading CLEVR dataset..."
+#wget https://s3-us-west-1.amazonaws.com/clevr/CLEVR_v1.0.zip
+#unzip CLEVR_v1.0.zip
+#rm CLEVR_v1.0.zip
 
 echo "Downloading CLEVR-mini dataset..."
 wget http://nsvqa.csail.mit.edu/assets/CLEVR_mini.zip
